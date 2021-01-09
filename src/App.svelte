82225f1c9a5697adf110b0b0ce086118832e22a9
knightss@catlin.edu <knightss@catlin.edu>
2021-01-09 00:51:39 -0800

    Clean up svelte even more.

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,33 @@
 <script>
-	export let name;
+	// Code for future use for calling functions in the content script.
+	// window.onload = function() {
+
+	// 	document.querySelector('button').addEventListener('click', refreshStyles)
+
+	// 	function refreshStyles() {
+	// 		console.log('test')
+	// 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	// 			chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+	// 			console.log(response.farewell);
+	// 			});
+	// 		});
+	// 	}
+	// }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>cgst</h1>
+	<p>hi nate</p>
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
 		width: 200px;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		padding-top: 0px;
+		margin-top: 0px;
 	}
 </style>

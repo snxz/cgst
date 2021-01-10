851b9e104c39160f6653ed2eaf33db82785fab0d
knightss@catlin.edu <knightss@catlin.edu>
2021-01-10 00:14:08 -0800

    Remove comment.

--- src/components/ThemeElement.svelte
+++ src/components/ThemeElement.svelte
@@ -14,12 +14,6 @@
     }
 
     async function applyStyle() {
-        // console.log(id)
-        // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        //     chrome.tabs.sendMessage(tabs[0].id, {action: "set", theme: id}, function(response) {
-        //     console.log(response);
-        //     });
-        // });
         await chrome.storage.local.set({ current: id })
     }
 </script>
